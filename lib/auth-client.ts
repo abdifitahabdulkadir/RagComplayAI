13e9b66debd8bead0b9ee76ccaac2b89d5f81b43
Abdifitah Abdulkadir <abdifitahabdulkadir22@gmail.com>
2025-11-09 22:34:55 +0300

    fix auth not working

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { emailOTPClient } from "better-auth/client/plugins";
 import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
   basePath: "/api/auth",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [nextCookies(), emailOTPClient()],
 });
